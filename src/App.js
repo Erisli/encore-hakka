f80a9b2397eda979caf5408450961a032f3df438
Erisli <42971850+Erisli@users.noreply.github.com>
2022-06-03 20:57:18 -0400

    Update environment

--- src/App.js
+++ src/App.js
@@ -9,9 +9,6 @@ import Combo from './components/Combo/Combo';
 import Trays from './components/Trays/Trays';
 import Footer from './components/Footer/Footer';
 import React from "react";
-import { render } from "react-dom";
-import { useWindowSize } from "./Hooks/useWindowSize";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -25,6 +22,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Slider />}/>
           <Route path="/home" element={<Slider />}/>
+          <Route path="/Encore-Hakka" element={<Slider />}/>
           <Route path="menu" element={<Menu />}/>
           <Route path="lunch" element={<Lunch />}/>
           <Route path="coupons" element={<Coupons />}/>
